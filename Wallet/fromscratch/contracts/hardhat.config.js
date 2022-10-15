98a92e57aa515fbeb949cff94d4a87a74a9fcc9a
0xturboblitz <florent.tavernier@gmail.com>
2022-10-15 17:42:44 +0200

    fix

--- Wallet/fromscratch/contracts/hardhat.config.js
+++ Wallet/fromscratch/contracts/hardhat.config.js
@@ -3,8 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "goerli",
+  // defaultNetwork: "goerli",
   networks: {
+    hardhat: {
+      forking: {
+        url: "https://polygon-rpc.com",
+      }
+    },
     fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       accounts: ["2ccfe123b7e5a3f6672cc6956f3c25b7fa25df1365cf0879a207756a68ac3f8b"]
--- Wallet/fromscratch/contracts/scripts/deploy.js
+++ Wallet/fromscratch/contracts/scripts/deploy.js
@@ -8,25 +8,29 @@ const hre = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 async function main() {
-  const addressWithTokens = "0x9bdB521a97E95177BF252C253E256A60C3e14447"
-  await network.provider.send("hardhat_impersonateAccount", [addressWithTokens])
-  const impersonatedSigner = await ethers.getSigner(addressWithTokens) 
-  
   const Looping = await hre.ethers.getContractFactory("Looping");
   const looping = await Looping.deploy();
 
   await looping.deployed();
-
   console.log(`Looping deployed to ${looping.address}`);
 
   const usdcAddr = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
   const wethAddr  = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+  
+  let weth = await hre.ethers.getContractAt("IERC20", wethAddr);
 
-  console.log("Sending weth ton contract...")
-  let weth = await hre.ethers.getContractAt("IERC20", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619");
-  weth = await weth.connect(impersonatedSigner);
-  // const sendWethTx = await weth.connect(impersonatedSigner).transfer(looping.address, "10000000000000000");
-  const sendWethTx = await weth.transfer(looping.address, "10000000000000000");
+  console.log("Impersonating account...")
+  const addressWithTokens = "0x9bdB521a97E95177BF252C253E256A60C3e14447"
+  const impersonatedSigner = await hre.ethers.getImpersonatedSigner(addressWithTokens);
+  
+  console.log("sending tx...")
+  
+  let unsignedTx = await weth.populateTransaction["transfer"](looping.address, "10000000000000000");
+  console.log("unsignedTx:", unsignedTx)
+  unsignedTx.from = "0x9bdB521a97E95177BF252C253E256A60C3e14447"
+
+  console.log("Sending weth to contract...")
+  const sendWethTx = await impersonatedSigner.sendTransaction(unsignedTx);
   console.log(sendWethTx.hash);
   await sendWethTx.wait();
   console.log("Done")
@@ -39,6 +43,10 @@ async function main() {
 
 
 
+}
+
+async function main2() {
+  console.log(await hre.ethers.provider.getNetwork())
 }
 
 // We recommend this pattern to be able to use async/await everywhere
